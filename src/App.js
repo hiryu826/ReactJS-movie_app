33ad3a4e91bcc75b6c7314eb099114dfbddca869
HiRyu <hiryu0826@gmail.com>
2020-04-07 23:35:05 +0900

    (2020.04.07) JSX + Props
    (2020.04.07) JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+function Food(props) {
+  return <h1>Hi {props.fav}</h1>;
+}
+
 function App() {
   return (
     <div>
       <h1>hello</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
