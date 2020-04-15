386abaf0c8c704dfc7c36af0611d453f63b7f6d6
HiRyu <hiryu0826@gmail.com>
2020-04-15 23:16:31 +0900

    (2020.04.15) JS Map
    (2020.04.15) JS Map

--- src/App.js
+++ src/App.js
@@ -4,18 +4,53 @@ import React from "react";
 //   return <h1>Hi {props.fav}</h1>;
 // }
 
-function Food({ fav }) {
-  return <h5>Try {fav}</h5>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2> This is {name} </h2>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  // Food의 object 배열
+  {
+    name: "Image 1",
+    image:
+      "https://cdn.pixabay.com/photo/2020/04/13/13/25/water-5038379_960_720.jpg",
+  },
+  {
+    name: "Image 2",
+    image:
+      "https://cdn.pixabay.com/photo/2020/04/09/11/42/dogs-5021085_960_720.jpg",
+  },
+  {
+    name: "Image 3",
+    image:
+      "https://cdn.pixabay.com/photo/2020/04/01/22/14/lover-4992877_960_720.png",
+  },
+  {
+    name: "Image 4",
+    image:
+      "https://cdn.pixabay.com/photo/2020/03/10/16/47/moon-4919501_960_720.jpg",
+  },
+  {
+    name: "Image 5",
+    image:
+      "https://cdn.pixabay.com/photo/2020/04/06/11/22/seedling-5009286_960_720.jpg",
+  },
+];
 
 function App() {
   return (
+    // 모든 component(function component, food component) 들을 동적으로(Dynamic) rendering 한다. (js object의 lsit를 가지고 온다.)
+    // map은 function을 취한 후 그 function을 array의 각 itmem에 적용해준다.
+    // dish는 object 이다.
     <div>
-      <h1>hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
