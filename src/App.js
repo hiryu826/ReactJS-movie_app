2afcbb49837d7ca1df5ede7c946c12b142d9e621
HiRyu <hiryu0826@gmail.com>
2020-04-16 23:08:34 +0900

    (2020.04.16) Check PropTypes
    (2020.04.16) Check PropTypes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //   return <h1>Hi {props.fav}</h1>;
 // }
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h2> This is {name} </h2>
+      <h4> {rating}/5.0 </h4>
       <img src={picture} alt={name} />
     </div>
   );
 }
+
+// Props 정합성 체크
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 // Food의 object 배열
 const foodILike = [
@@ -19,30 +28,35 @@ const foodILike = [
     name: "Image 1",
     image:
       "https://cdn.pixabay.com/photo/2020/04/13/13/25/water-5038379_960_720.jpg",
+    rating: 5,
   },
   {
     id: 2,
     name: "Image 2",
     image:
       "https://cdn.pixabay.com/photo/2020/04/09/11/42/dogs-5021085_960_720.jpg",
+    rating: 4.9,
   },
   {
     id: 3,
     name: "Image 3",
     image:
       "https://cdn.pixabay.com/photo/2020/04/01/22/14/lover-4992877_960_720.png",
+    rating: 4.7,
   },
   {
     id: 4,
     name: "Image 4",
     image:
       "https://cdn.pixabay.com/photo/2020/03/10/16/47/moon-4919501_960_720.jpg",
+    rating: 5.5,
   },
   {
     id: 5,
     name: "Image 5",
     image:
       "https://cdn.pixabay.com/photo/2020/04/06/11/22/seedling-5009286_960_720.jpg",
+    rating: 5.3,
   },
 ];
 
@@ -53,7 +67,12 @@ function App() {
     // dish는 object 이다.
     <div>
       {foodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
