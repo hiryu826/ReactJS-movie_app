56099bf9a65726dc1869e150b64446f5a924be14
HiRyu <hiryu0826@gmail.com>
2020-04-16 22:45:38 +0900

    (2020.04.16) JS Map
    (2020.04.16) JS Map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-// function Food(props) {
 //   return <h1>Hi {props.fav}</h1>;
 // }
 
@@ -8,34 +7,39 @@ function Food({ name, picture }) {
   return (
     <div>
       <h2> This is {name} </h2>
-      <img src={picture} />
+      <img src={picture} alt={name} />
     </div>
   );
 }
 
+// Food의 object 배열
 const foodILike = [
-  // Food의 object 배열
   {
+    id: 1,
     name: "Image 1",
     image:
       "https://cdn.pixabay.com/photo/2020/04/13/13/25/water-5038379_960_720.jpg",
   },
   {
+    id: 2,
     name: "Image 2",
     image:
       "https://cdn.pixabay.com/photo/2020/04/09/11/42/dogs-5021085_960_720.jpg",
   },
   {
+    id: 3,
     name: "Image 3",
     image:
       "https://cdn.pixabay.com/photo/2020/04/01/22/14/lover-4992877_960_720.png",
   },
   {
+    id: 4,
     name: "Image 4",
     image:
       "https://cdn.pixabay.com/photo/2020/03/10/16/47/moon-4919501_960_720.jpg",
   },
   {
+    id: 5,
     name: "Image 5",
     image:
       "https://cdn.pixabay.com/photo/2020/04/06/11/22/seedling-5009286_960_720.jpg",
@@ -49,7 +53,7 @@ function App() {
     // dish는 object 이다.
     <div>
       {foodILike.map((dish) => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
       ))}
     </div>
   );
