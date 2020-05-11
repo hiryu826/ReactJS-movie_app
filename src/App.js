03c95b6f745835adeb04a83be0c1b0d0a500a55b
HiRyu <hiryu0826@gmail.com>
2020-05-11 22:49:16 +0900

    (2020.05.11) Route
    (2020.05.11) Route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";
-//import Home from "./routes/Home";
+import Home from "./routes/Home";
 import About from "./routes/About";
 
 function App() {
   return (
     <HashRouter>
-      <Route path="/home">
-        <h1> home </h1>
-      </Route>
-      <Route path="/home/test">
-        <h1> test </h1>
-      </Route>
-      <Route path="/home/test/test2">
-        <h1> test2 </h1>
-      </Route>
+      <Route path="/home" exact={true} component={Home} />
       <Route path="/about" component={About} />
     </HashRouter>
   );
