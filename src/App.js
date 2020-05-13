5de38463341801775efa965327849201a2c78e2b
HiRyu <hiryu0826@gmail.com>
2020-05-13 17:56:16 +0900

    (2020.05.13) Send movie property to Detail Page
    (2020.05.13) Send movie property to Detail Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <HashRouter>
       <Navigation />
       <Route path="/" exact={true} component={Home} />
-      <Route path="/about" exact={true} component={About} />
+      <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
     </HashRouter>
   );
 }
