9fb6b4b6fa436acabf780ad10676db8ee664f102
HiRyu <hiryu0826@gmail.com>
2020-05-14 23:22:18 +0900

    (2020.05.14) URL Redirect
    (2020.05.14) URL Redirect

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,21 @@
 import React from "react";
 
-function Detail(props) {
-  console.log(props);
-  return <span>Test Detail Page</span>;
+class Detail extends React.Component {
+  componentDidMount() {
+    console.log(this.props);
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/"); //history - url 변경 요소, movie-detail 경로 강제 입력 시 Home으로 Redirect
+    }
+  }
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
+  }
 }
 
 export default Detail;
